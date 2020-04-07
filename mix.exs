85938607fa09245d1bc860d838a3981d35cb7f5a
Bastien Rivière <babathriviere@gmail.com>
2020-04-07 23:39:49 +0200

    Setup lint tools for Elixir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Unixbot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      dialyzer: [
+        plt_add_deps: :transitive
+      ]
     ]
   end
 
@@ -21,8 +25,14 @@ defmodule Unixbot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: ["credo", "dialyzer"]
     ]
   end
 end
