dd894ed8f049391382e3cb437fd48c07ed65a4d9
Bastien Rivière <babathriviere@gmail.com>
2020-04-10 17:58:21 +0200

    Improve documentation from inch suggestions.

--- lib/reddit.ex
+++ lib/reddit.ex
@@ -32,18 +32,33 @@ defmodule Reddit do
     get("/api/v1/me")
   end
 
+  @type time :: :hour | :day | :week | :month | :year | :all
+
   @type listing :: [
-          t: :hour | :day | :week | :month | :year | :all,
+          t: time,
           after: String.t(),
           before: String.t(),
           limit: non_neg_integer(),
           count: non_neg_integer()
         ]
 
+  @type sort :: :top | :hot | :new | :rising
+
   @doc """
-  Query a list of posts from the subreddit.
+  Query a list of posts from a subreddit.
+
+  # Params
+
+  - `name`: subreddit to list.
+  - `sort`: order by sort type
+  - `params`: listing parameters used for pagination or sort by time.
+
+  # Example
+
+      iex> Reddit.subreddit("unixporn", :top, t: day)
+      {:ok, %Reddit.Listing{after: nil, before: nil, children: [...]}}
   """
-  @spec subreddit(String.t(), :top | :hot | :new | :rising, listing) ::
+  @spec subreddit(String.t(), sort, listing) ::
           {:ok, Reddit.Listing.t(Reddit.Post.t())} | nil
   def subreddit(name, sort \\ :top, params \\ []) do
     case get("/r/#{name}/#{sort}", [], params: params) do
